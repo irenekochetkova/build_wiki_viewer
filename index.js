1a2ed8d970e03a8e0b6383c53801a07614a937cd
Irina <irenekochetkova@gmail.com>
2018-05-05 10:38:12 -0700

    added the javascript code to call Wikipedia API

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function getRandom() {
     window.open("https://en.wikipedia.org/wiki/Special:Random");
 }
 
+$.ajax( {
+    url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
+    
+    dataType: 'jsonp',
+   
+   
+    success: function(data) {
+       console.log(data);
+    }
+} );
