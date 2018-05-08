b561616e6696d2759acdfe925bf4b9838eec10f2
Irina <irenekochetkova@gmail.com>
2018-05-08 12:26:57 -0700

    changed url

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-
-
-
 function getRandom() {
     window.open("https://en.wikipedia.org/wiki/Special:Random");
 }
 
-$.ajax( {
-    url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
-    
-    dataType: 'jsonp',
+// $(document).ready(function() {
+  $("#input-search").on("click", function() {
+     $("ul").empty();
+    var searchInput = $("#textName").val();
+   
+    $.ajax ({
+     
+  
+      url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&prop=extracts&exsentences=1&search="+searchInput+"&limit=8&callback=?",
+
+       dataType: "json",
    
    
     success: function(data) {
        console.log(data);
+      
     }
-} );
+    });
+  });
+// });
