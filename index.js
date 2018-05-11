08e72b092751ed7ca8b42a6b86081407b73aea42
Irina <irenekochetkova@gmail.com>
2018-05-10 21:58:26 -0700

    added conditinal

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 function getRandom() {
-    window.open("https://en.wikipedia.org/wiki/Special:Random");
+  window.open("https://en.wikipedia.org/wiki/Special:Random");
 }
 
 $(document).ready(function() {
   $(".button").keypress(function() {
-     $("#link").empty();
-    var searchInput = $("#textName").val();
-   
-    $.ajax ({
-     
-  
-      url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search="+searchInput,
+    $("#link").empty();
+      var searchInput = $("#textName").val();   
+        $.ajax ({  
+          url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search="+searchInput,
+          dataType: "json",   
+          success: function(data) {
+            for (var i = 0; i < 10; i++) {
+              var searchUrl = data[3][i];
+              var title = data[1][i];
+              var quote = data[2][i];
 
-       dataType: "json",
-   
-   
-    success: function(data) {
-       console.log(data);
-
-       for (var i = 0; i < 10; i++) {
-        var searchUrl = data[3][i];
-        var title = data[1][i];
-        var quote = data[2][i];
-
-       var html = "<div ><a  href="+searchUrl+"><h4>"+title+"</h4>"+quote+"</a></div>";
-         $("#link").append(html);
-       }
-    }
+                if (searchUrl) {
+                  var html = "<div ><a  href="+searchUrl+"><h4>"+title+"</h4>"+quote+"</a></div>";
+                  $("#link").append(html);
+                }
+            }
+          }
     });
   });
 });
